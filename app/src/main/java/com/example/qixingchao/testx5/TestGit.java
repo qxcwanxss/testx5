230012a8b62ee9ce55a638c6e1d871f8644083d1
qixingchao <970188529@qq.com>
2019-01-16 15:44:55 +0800

    add file TestGit.java

---
+++ app/src/main/java/com/example/qixingchao/testx5/TestGit.java
@@ -0,0 +1,13 @@
+package com.example.qixingchao.testx5;
+
+import android.util.Log;
+
+/**
+ * Created by qixingchao on 2019/1/16.
+ */
+
+public class TestGit {
+    public TestGit(){
+        Log.i("AAA","add file1");
+    }
+}
